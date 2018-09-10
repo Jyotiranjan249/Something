e2fafca4c540e0fdac498c634c146899e2c1d4f9
Jyotiranjan249 <vssut.jyotiranjan249@gmail.com>
2018-09-10 22:47:59 +0530

    first commit

---
+++ src/app/heros/heros.component.ts
@@ -0,0 +1,43 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero} from '../hero'
+import {HEROES} from '../mock-heroes';
+import {HeroService} from '../hero.service'
+@Component({
+  selector: 'app-heros',
+  templateUrl: './heros.component.html',
+  styleUrls: ['./heros.component.css']
+})
+export class HerosComponent implements OnInit {
+  // hero : Hero = {
+  //   id : 1,
+  //   name:'windstrom'
+  // }
+  heroes : Hero[];
+  //selectedHero : Hero ;
+  constructor(private heroservice:HeroService) {
+
+   }
+
+  ngOnInit() {
+    this.getHeroes()
+  }
+  
+  // onSelect(hero:Hero): void {
+  //   this.selectedHero = hero;
+  // }
+  getHeroes():void{
+    this.heroservice.getHeroes().subscribe(heroes=>this.heroes= heroes)
+  }
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroservice.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroservice.deleteHero(hero).subscribe();
+  }
+}
